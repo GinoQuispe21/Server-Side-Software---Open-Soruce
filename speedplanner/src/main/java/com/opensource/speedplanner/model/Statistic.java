4d6b645114a397d35064564ff1a2408b4f4739b1
Grecia Guerrero <greciaguerreroa@gmail.com>
2020-05-09 18:01:49 -0500

    Statistic class created

---
+++ speedplanner/src/main/java/com/opensource/speedplanner/model/Statistic.java
@@ -0,0 +1,23 @@
+package com.opensource.speedplanner.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "statistics")
+@Getter
+@Setter
+public class Statistic {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private int code;
+
+    @NotNull
+    @NotBlank
+    private String Info;
+}
