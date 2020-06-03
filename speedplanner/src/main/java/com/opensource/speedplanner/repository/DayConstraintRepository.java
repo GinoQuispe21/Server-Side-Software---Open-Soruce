9d23f46990d868aa408dcf3a3b45f7093870b142
cjmazzarri <cjmazzarri@gmail.com>
2020-06-02 19:13:56 -0500

    Added @Repository annotation

--- speedplanner/src/main/java/com/opensource/speedplanner/repository/DayConstraintRepository.java
+++ speedplanner/src/main/java/com/opensource/speedplanner/repository/DayConstraintRepository.java
@@ -2,5 +2,7 @@ package com.opensource.speedplanner.repository;
 
 import com.opensource.speedplanner.model.DayConstraint;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DayConstraintRepository extends JpaRepository<DayConstraint, Long> { }
--- speedplanner/src/main/java/com/opensource/speedplanner/repository/ProfessorConstraintRepository.java
+++ speedplanner/src/main/java/com/opensource/speedplanner/repository/ProfessorConstraintRepository.java
@@ -2,5 +2,7 @@ package com.opensource.speedplanner.repository;
 
 import com.opensource.speedplanner.model.ProfessorConstraint;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProfessorConstraintRepository extends JpaRepository<ProfessorConstraint, Long> { }
