e075bad685d78f68e4620a888a7215c6e62ec3df
GinoQuispe21 <gino_21quispe@outlook.com>
2020-06-04 17:50:40 -0500

    Correccion StatisticService

--- speedplanner/src/main/java/com/opensource/speedplanner/service/StatisticServiceImpl.java
+++ speedplanner/src/main/java/com/opensource/speedplanner/service/StatisticServiceImpl.java
@@ -1,31 +1,52 @@
 package com.opensource.speedplanner.service;
 
+import com.opensource.speedplanner.exception.ResourceNotFoundException;
 import com.opensource.speedplanner.model.Statistic;
 import com.opensource.speedplanner.repository.StatisticRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
-
 public class StatisticServiceImpl implements StatisticService{
-
     @Autowired
     private StatisticRepository statisticRepository;
 
-
     @Override
-    public Statistic createStatistic(Statistic statistic) {
-        return null;
-    }
+    public Statistic createStatistic(Statistic statistic) { return statisticRepository.save(statistic); }
 
     @Override
     public Statistic getStatisticById(Long statisticId) {
-        return null;
+        return statisticRepository.findById(statisticId)
+                .orElseThrow(() -> new ResourceNotFoundException("Statistic", "Id", statisticId));
+    }
+
+    @Override
+    public Statistic updateStatistic (Long statisticId, Statistic statisticDetails) {
+        return statisticRepository.findById(statisticId).map(statistic-> {
+            statistic.setMenPercentageInPeriod(statisticDetails.getMenPercentageInPeriod());
+            statistic.setWomenPercentageInPeriod(statisticDetails.getWomenPercentageInPeriod());
+            statistic.setRegisteredStudentsInPeriod(statisticDetails.getRegisteredStudentsInPeriod());
+            statistic.setLearningProgram(statisticDetails.getLearningProgram());
+            return statisticRepository.save(statistic);
+        }).orElseThrow(() -> new ResourceNotFoundException("Statistic", "Id", statisticId));
     }
 
     @Override
     public ResponseEntity<?> deleteStatistic(Long statisticId) {
-        return null;
+        Statistic statistic = statisticRepository.findById(statisticId)
+                .orElseThrow(() -> new ResourceNotFoundException("Statistic", "Id", statisticId));
+        statisticRepository.delete(statistic);
+        return ResponseEntity.ok().build();
+    }
+
+    @Override
+    public Page<Statistic> getAllStatistic(Pageable pageable) {
+        return statisticRepository.findAll(pageable);
     }
 }
+
+
+
