df9cda4df4cfea14461428a84b225b9fefd694f7
vilaron <49617031+vilaron@users.noreply.github.com>
2020-06-03 22:10:40 -0500

    Added Profile-Role-Section-SectionRequest

---
+++ speedplanner/src/main/java/com/opensource/speedplanner/service/SectionService.java
@@ -0,0 +1,23 @@
+package com.opensource.speedplanner.service;
+
+import com.opensource.speedplanner.model.Section;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+
+import java.awt.print.Pageable;
+
+/*
+    Page<Comment> getAllCommentsByPostId(Long postId, Pageable pageable);
+    Comment getCommentByIdAndPostId(Long postId, Long commentId);
+    Comment createComment(Long postId, Comment comment);
+    Comment updateComment(Long postId, Long commentId, Comment commentDetails);
+    ResponseEntity<?> deleteComment(Long postId, Long commentId);*/
+
+public interface SectionService {
+    Page<Section> getAllSections(Pageable pageable);
+    Page<Section> getAllSectionsByCourseId (Long courseId , Pageable pageable);
+    Section getSectionByIdAndCourseId (Long courseId , Long sectionId);
+    Section createSection(Long courseId , Section section);
+    Section updateSection(Long courseId , Long sectionId , Section section);
+    ResponseEntity<?> deleteSection(Long courseId , Long sectionId );
+}
